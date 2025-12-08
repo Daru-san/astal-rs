261153f1304da8b0a90721bb88b085da293be69f
Daru <mica.tui@proton.me>
2025-12-08 15:52:45 +0200

    feat: generate astal-auth

---
+++ crates/astal-auth/src/lib.rs
@@ -0,0 +1,17 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![allow(unsafe_op_in_unsafe_fn)]
+#![allow(unused_imports)]
+
+macro_rules! assert_initialized_main_thread {
+    () => {};
+}
+
+macro_rules! skip_assert_initialized {
+    () => {};
+}
+
+use astal_auth_sys as ffi;
+pub use auto::*;
+
+mod auto;
+pub mod prelude;
