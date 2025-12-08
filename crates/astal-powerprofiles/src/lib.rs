588cbd057e73f87697adf994f50f4c2230e01276
Daru <mica.tui@proton.me>
2025-12-08 16:32:35 +0200

    feat: generate astal-powerprofiles
    Does not build as of yet.

---
+++ crates/astal-powerprofiles/src/lib.rs
@@ -0,0 +1,18 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![allow(unsafe_op_in_unsafe_fn)]
+#![allow(deprecated)]
+#![allow(unused_imports)]
+
+macro_rules! assert_initialized_main_thread {
+    () => {};
+}
+
+use astal_power_profiles_sys as ffi;
+pub use auto::*;
+
+mod auto;
+pub mod prelude;
+
+pub mod functions {
+    pub use super::auto::functions::*;
+}
