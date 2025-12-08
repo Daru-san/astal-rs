2e8b9c5dddf540ec504cf76e7a8eaa98410c625b
Daru <mica.tui@proton.me>
2025-12-08 15:09:18 +0200

    feat: generate astal-battery

---
+++ crates/astal-battery/src/lib.rs
@@ -0,0 +1,18 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![allow(unsafe_op_in_unsafe_fn)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+
+macro_rules! assert_initialized_main_thread {
+    () => {};
+}
+
+macro_rules! skip_assert_initialized {
+    () => {};
+}
+
+use astal_battery_sys as ffi;
+pub use auto::*;
+
+mod auto;
+pub mod prelude;
