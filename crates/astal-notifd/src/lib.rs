0a95457e47ade776144ed4cce089508319bf0369
Daru <mica.tui@proton.me>
2025-12-08 16:18:05 +0200

    feat: generate astal-notifd

---
+++ crates/astal-notifd/src/lib.rs
@@ -0,0 +1,22 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![allow(unsafe_op_in_unsafe_fn)]
+#![allow(deprecated)]
+#![allow(unused_imports)]
+
+macro_rules! assert_initialized_main_thread {
+    () => {};
+}
+
+macro_rules! skip_assert_initialized {
+    () => {};
+}
+
+use astal_notifd_sys as ffi;
+pub use auto::*;
+
+mod auto;
+pub mod prelude;
+
+pub mod functions {
+    pub use super::auto::functions::*;
+}
